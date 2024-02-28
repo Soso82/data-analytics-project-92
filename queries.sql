3aa08cfdf5dc4a8a6d2910d4dc418d4c4991f725
Soso82 <soso82@mail.ru>
2024-02-28 21:04:25 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,2 +1,2 @@
 --запрос, который считает общее количество покупателей. Название колонки customers_count
-select count(customer_id) as customs_count  from customers; 
+select count(customer_id) as customs_count from customers; 
